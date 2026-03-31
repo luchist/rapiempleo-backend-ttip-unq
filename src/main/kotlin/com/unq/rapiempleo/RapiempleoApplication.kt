cbf2bab2d8d5c884c3f891ce6a4682c8f10bf03a
luchist <lucho_9292@live.com>
2026-03-31 20:28:05 -0300

    initial commit

---
+++ src/main/kotlin/com/unq/rapiempleo/RapiempleoApplication.kt
@@ -0,0 +1,11 @@
+package com.unq.rapiempleo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RapiempleoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RapiempleoApplication>(*args)
+}
---
+++ src/test/kotlin/com/unq/rapiempleo/RapiempleoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unq.rapiempleo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RapiempleoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
