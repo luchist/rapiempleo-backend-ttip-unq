cfb84e8142c4e7d9e46cd13160d615e6d95b68aa
luchist <lucho_9292@live.com>
2026-04-12 22:08:55 -0300

    feat: CORS enabled for frontend urls

---
+++ src/main/kotlin/com/unq/rapiempleo/config/CorsConfig.kt
@@ -0,0 +1,14 @@
+package com.unq.rapiempleo.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4173", "http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+}
