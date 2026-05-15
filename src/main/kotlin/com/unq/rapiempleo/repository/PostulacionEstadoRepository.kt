98238c25946f406694c63640543f012e7715b5e1
luchist <lucho_9292@live.com>
2026-05-15 20:17:57 -0300

    chore: adds preloaded offers for status board, fixed repository query

--- src/main/kotlin/com/unq/rapiempleo/repository/PostulacionEstadoRepository.kt
+++ src/main/kotlin/com/unq/rapiempleo/repository/PostulacionEstadoRepository.kt
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query
 interface PostulacionEstadoRepository : JpaRepository<PostulacionEstado, Long> {
     @Modifying
     @Transactional
-    @Query(value = "ALTER TABLE oferta AUTO_INCREMENT = 1", nativeQuery = true)
+    @Query(value = "ALTER TABLE postulacion_estado AUTO_INCREMENT = 1", nativeQuery = true)
     fun resetIdPostulacionEstado()
 
     fun findByPostulante(postulante: Postulante): List<PostulacionEstado>
