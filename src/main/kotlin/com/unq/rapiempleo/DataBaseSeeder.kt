0076fa83292f1f5ac75258772beb0402193a29c6
Carlos7410 <carlos_rivero7410@hotmail.com>
2026-06-25 23:35:54 -0300

    Fixed test of DataBaseSeeder

--- src/main/kotlin/com/unq/rapiempleo/DataBaseSeeder.kt
+++ src/main/kotlin/com/unq/rapiempleo/DataBaseSeeder.kt
@@ -108,9 +108,9 @@ class DataBaseSeeder(
         ofertas[9].ofertante = ofertanteTest1
 
         ofertas[6].ofertante = ofertanteTest1
-        ofertas[7].ofertante = ofertanteTest1
+        ofertas[7].ofertante = ofertanteTest2
         ofertas[8].ofertante = ofertanteTest1
-        ofertas[10].ofertante = ofertanteTest2
+        ofertas[10].ofertante = ofertanteTest1
 
         ofertas[0].ofertante = ofertanteTest3
         ofertas[5].ofertante = ofertanteTest3
